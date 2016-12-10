cfe6cd8ca2a08283b6a8e7b054fe027b3267e447
Jenkins <jenkins@localhost.localdomain>
2016-12-10 15:59:50 +0000

    Bring up support for wt86528

---
+++ omni_wt86528.mk
@@ -0,0 +1,43 @@
+#
+# Copyright 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+LOCAL_PATH := device/wingtech/wt86528
+
+# Charger
+PRODUCT_PACKAGES += charger charger_res_images
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dt.img:dt.img \
+    $(LOCAL_PATH)/kernel:kernel \
+    $(LOCAL_PATH)/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_PATH)/twrp.fstab:root/etc/twrp.fstab
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := wt86528
+PRODUCT_NAME := omni_wt86528
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := K31-t3
+PRODUCT_MANUFACTURER := Wingtech
+
+# enable stock zip packages flash
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.build.product=K31-t3 \
+    ro.product.device=K31-t3
